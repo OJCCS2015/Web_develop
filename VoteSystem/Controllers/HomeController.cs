1a703d67075d9a5621cc5553afce1a77c986857b
Zhicai Dai <daizhicai@foxmail.com>
2018-04-05 23:33:45 +0800

    首次个人分支提交

---
+++ VoteSystem/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VoteSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Homes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
