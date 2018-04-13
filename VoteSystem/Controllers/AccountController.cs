36001d24b5943cef00a86df9ad7db68782d0aa90
chrisqu97 <392788234@qq.com>
2018-04-13 20:29:46 +0800

    增加登录和注册

--- VoteSystem/Controllers/AccountController.cs
+++ VoteSystem/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VoteSystem.Dao;
+using VoteSystem.Models;
 
 namespace VoteSystem.Controllers
 {
@@ -13,9 +15,50 @@ namespace VoteSystem.Controllers
         {
             return View();
         }
-        public ActionResult Login()
-        {
+        [HttpPost]
+        public ActionResult Register(RegisterModel model) {
+            if (!ModelState.IsValid) {
+                ViewBag.res = "提交失败";
+                return View(model);
+            }
+            UserDao userdao = new UserDao();
+            User user = new User();
+            user.userNick = model.userNick;
+            user.userEmail = model.userEmail;
+            user.userPsw = model.userPsw;
+            if (userdao.getByUserEmail(user.userEmail) != null)
+            {
+                ViewBag.res = "用户已存在";
+                return View(model);
+            }else {
+                userdao.addUser(user);
+                ViewBag.res = "注册成功";
+                return View(model);
+            }
+        }
+        public ActionResult Login() {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid) {
+                ViewBag.res = "提交失败";
+                return View(model);
+            }
+            UserDao userdao = new UserDao ();
+            User user = new User();
+            user.userEmail = model.userEmail;
+            user.userPsw = model.userPsw;
+            if (userdao.checkUser(user)!=null) {
+                ViewBag.res = "登录成功";
+            }
+            else
+                ViewBag.res = "登录失败";
+            return View();
+        }
+        public Boolean checkByUserEmail() {
+            return false;
         }
 
     }
